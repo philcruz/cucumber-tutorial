e480a576d1646830ab993c37da135c65bde1e25c
Phil Cruz <phil@philcruz.com>
2015-10-21 10:00:27 -0700

    add create_widget_steps.js with all steps pending

---
+++ tests/cucumber/features/step_definitions/create_widget_steps.js
@@ -0,0 +1,25 @@
+(function () {
+
+	'use strict';
+
+	module.exports = function () {
+
+		this.Given(/^I am logged in$/, function () {
+		pending();
+		});
+
+		this.When(/^I fill in the name with "([^"]*)"$/, function (arg1) {
+			pending();
+		});
+
+		this.When(/^I click the button "([^"]*)"$/, function (arg1) {
+			pending();
+		});
+
+		this.Then(/^I should see a widget named "([^"]*)"$/, function (arg1) {
+			pending();
+		});
+
+	}
+
+})();
